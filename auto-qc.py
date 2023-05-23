199c14dfaa2c7ecff5bc1811b823d878b55eca5d
Fabian Bryant <134044947+fabianbryant@users.noreply.github.com>
2023-05-22 22:17:53 -0500

    Update auto-qc.py
    - Commented out feature that searches mfg. devices table b/c bug causing incorrect text to print (will resolve if I can)
    
    - Structured all parts of program as part of some function, including a main( ) function
    
    - Added "if name == main" to run code
    
    - Updated in-script notes/features to add

--- auto-qc.py
+++ auto-qc.py
@@ -2,7 +2,6 @@ import sys
 import csv
 import time
 import queue
-# ~ import logging
 import threading
 
 from datetime import datetime
@@ -19,10 +18,18 @@ from selenium.common.exceptions import (
     ElementClickInterceptedException as InterceptErr,
 )
 
-# ~ Last updated 5/21/23
-# ~ Need to add: logging, thread + function for finding/adding cameras in Viewer,
-# ~ Code still needs to be tested at QC Station
-# ~ Version of this code w/o Queues or Threads is used at QC currently
+# ~ Last updated 5/22/23
+
+# ~ Tested and did some debugging. Removed feature that searches
+# ~ mfg devices table, as it won't print the correct HTML element.
+# ~ Upon search result, it'll just print out the C-DUM-007 that
+# ~ shows up on the first page. Not sure how to fix, so cmt'd out.
+
+# ~ Features to add:
+#   - better method of recording data than CSV file (SQL?)
+#   - auto-find/add cameras function (if possible)
+#   - logging?
+
 
 """
 Program was scripted by Fabian Bryant, Manufacturing Technician. (fbryant@een.com)
@@ -50,7 +57,9 @@ def getAdmin():
 
         try:
             Wait(wd1, 2.5).until(
-                ec.visibility_of_element_located((By.NAME, "search_type"))
+                ec.visibility_of_element_located(
+                    (By.NAME, "search_type")
+                )
             )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
             pass
@@ -59,12 +68,14 @@ def getAdmin():
 
         try:
             usr = Wait(wd1, 7.5).until(
-                ec.visibility_of_element_located((By.NAME, "username"))
+                ec.visibility_of_element_located(
+                    (By.NAME, "username")
+                )
             )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
             continue
         else:
-            usr.send_keys(USERNAME)
+            usr.send_keys("USERNAME")
             while True:
                 cont = input(
                     "\nEnter 'ok' when login is authenticated to continue: "
@@ -95,7 +106,11 @@ def getViewer(acct):
         wd2.get("https://c014.eagleeyenetworks.com/#/dash")
 
         try:
-            Wait(wd2, 2.5).until(ec.presence_of_element_located((By.NAME, "search")))
+            Wait(wd2, 2.5).until(
+                ec.presence_of_element_located(
+                    (By.NAME, "search")
+                )
+            )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
             pass
         else:
@@ -103,7 +118,9 @@ def getViewer(acct):
 
         try:
             usr = Wait(wd2, 7.5).until(
-                ec.visibility_of_element_located((By.ID, "email"))
+                ec.visibility_of_element_located(
+                    (By.ID, "email")
+                )
             )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
             continue
@@ -112,7 +129,7 @@ def getViewer(acct):
             usr.send_keys(acct)
             pwd = wd2.find_element(By.ID, "password1")
             pwd.clear()
-            pwd.send_keys(PASSWORD)
+            pwd.send_keys("PASSWORD")
             wd2.find_element(By.ID, "login_button").click()
             break
 
@@ -144,7 +161,9 @@ def searcher(in_q, out_q):
         try:
             Select(
                 Wait(wd1, 5).until(
-                    ec.visibility_of_element_located((By.NAME, "search_type"))
+                    ec.visibility_of_element_located(
+                        (By.NAME, "search_type")
+                    )
                 )
             ).select_by_visible_text("Bridges and Cameras")
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
@@ -162,79 +181,79 @@ def searcher(in_q, out_q):
                 )
             )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
-            pass
-        else:
-            in_q.put(serial)
-            continue
-
-        try:
-            ip = Wait(wd1, 5).until(
+            try:
+                ip_addr = Wait(wd1, 5).until(
                     ec.visibility_of_element_located(
                         (By.XPATH, "//td[@id='bridge-ip_address']")
                     )
                 ).text
-        except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
-            continue
+            except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
+                continue
+            else:
+                serial = wd1.find_element(By.XPATH, "//td[@id='bridge-serial']").text
+                attach_id = wd1.find_element(By.XPATH, "//td[@id='bridge-connect_id']").text
+                firmware = wd1.find_element(By.XPATH, "//td[@id='bridge-firmware']").text
+                pass
         else:
-            serial = wd1.find_element(By.XPATH, "//td[@id='bridge-serial']").text
-            attach_id = wd1.find_element(By.XPATH, "//td[@id='bridge-connect_id']").text
-            firmware = wd1.find_element(By.XPATH, "//td[@id='bridge-firmware']").text
+            print("\nNo matches found for:", serial, "\n")
+            continue
+            
+            # wd1.get(
+                # "https://eenadmin.eagleeyenetworks.com/eenadmin/vms_admin/vms/manufactureddevice/"
+            # )
 
-            if not attach_id:
-                wd1.get(
-                    "https://eenadmin.eagleeyenetworks.com/eenadmin/vms_admin/vms/manufactureddevice/"
-                )
+            # try:
+                # srch = Wait(wd1, 5).until(
+                    # ec.visibility_of_element_located(
+                        # (By.NAME, "q")
+                    # )
+                # )
+            # except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
+                # in_q.put(serial)
+                # continue
+            # else:
+                # srch.clear()
+                # srch.send_keys(serial, Keys.RETURN)
 
-                try:
-                    search = Wait(wd1, 5).until(
-                        ec.visibility_of_element_located((By.NAME, "q"))
-                    )
-                except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
-                    in_q.put(serial)
-                    continue
-                else:
-                    search.clear()
-                    search.send_keys(serial, Keys.RETURN)
+            # try:
+                # pgn = Wait(wd1, 5).until(
+                        # ec.visibility_of_element_located(
+                            # (By.XPATH, "//p[@class='paginator']")
+                        # )
+                    # ).text
+            # except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
+                # in_q.put(serial)
+                # continue
+            # else:
+                # if pgn[0] == "0":
+                    # print("\nNo bridges found for:", serial, "\n")
+                    # continue
 
-                try:
-                    pgn = Wait(wd1, 5).until(
-                            ec.visibility_of_element_located(
-                                (By.XPATH, "//p[@class='paginator']")
-                            )
-                        ).text
-                except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
-                    in_q.put(serial)
-                    continue
-                else:
-                    if pgn[0] == "0":
-                        print("\nNo bridges found for:", serial, "\n")
-                        continue
+                # elif pgn[0] == "1":
+                    # try:
+                        # serial = Wait(wd1, 2.5).until(
+                            # ec.visibility_of_element_located(
+                                # (By.CLASS_NAME, "field-serialNumber")
+                            # )
+                        # ).text
 
-                    elif pgn[0] == "1":
-                        ip = "None"
-                        serial = wd1.find_element(
-                            By.XPATH, "//td[@class='field-serialNumber']"
-                        ).text
-                        attach_id = wd1.find_element(
-                            By.XPATH, "//td[@class='field-activationCode']"
-                        ).text
-                        firmware = "None"
-
-                    else:
-                        print("\nFound multiple bridges for:", serial, "\n")
-                        continue
-        finally:
-            print(
-                "\n\t\t",
-                ip,
-                "\n\n\t\t",
-                serial,
-                "\n\n\t\t",
-                attach_id,
-                "\n\n\t\t",
-                firmware,
-                "\n",
-            )
+                        # attach_id = Wait(wd1, 2.5).until(
+                            # ec.visibility_of_element_located(
+                                # (By.CLASS_NAME, "field-activationCode")
+                            # )
+                        # ).text
+                    # except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
+                        # continue
+                    # else:
+                        # ip_addr = "None"
+                        # firmware = "None"
+                # else:
+                    # print("\nFound multiple bridges for:", serial, "\n")
+                    # continue
+        print(
+            "\n\t\t", ip_addr, "\n\n\t\t", serial, 
+            "\n\n\t\t", attach_id, "\n\n\t\t", firmware, "\n"
+        )
 
         out_q.put((attach_id, serial))
 
@@ -258,8 +277,6 @@ def connecter(out_q):
     Connect function to be used by Thread 2.
     :return:
     """
-
-    getViewer(acct)
 
     tries = 0
 
@@ -286,7 +303,7 @@ def connecter(out_q):
                 )
             ).click()
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr, InterceptErr):
-            bridge.put(out_q)
+            out_q.put(bridge)
             continue
 
         try:
@@ -296,7 +313,7 @@ def connecter(out_q):
                 )
             ).click()
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr, InterceptErr):
-            bridge.put(out_q)
+            out_q.put(bridge)
             continue
 
         try:
@@ -306,69 +323,88 @@ def connecter(out_q):
                 )
             )
         except (NotFoundErr, TimeoutErr, StaleRefErr, SelectorErr):
-            bridge.put(out_q)
+            out_q.put(bridge)
             continue
         else:
             bridge_id.clear()
             bridge_id.send_keys(bridge[0])
-            bridge_name = wd2.find_element(By.XPATH, "//input[@id]='addBridgeName']")
+            bridge_name = wd2.find_element(By.XPATH, "//input[@id='addBridgeName']")
             bridge_name.clear()
             bridge_name.send_keys(bridge[1], Keys.TAB, Keys.TAB, Keys.RETURN)
         tries = 0
 
 
+def pickAcct():
+    global acct
+    
+    while True:
+        option = input(
+            "\nWhich Eagle Eye Viewer account will be used?\n"
+            "\n1. ACCOUNT  OR  2. ACCOUNT2  [ACC/ACC2]: "
+        ).upper()
+
+        if option == 1 or "ACC":
+            acct = "ACCOUNT"
+            break
+
+        elif option == 2 or "ACC2":
+            acct = "ACCOUNT2"
+            break
+
+        print("\nGot invalid response:", str(option), "\n")
+    return acct
+
+
+def makeObjects():
+    # ~ opts = webdriver.FirefoxOptions()
+    # ~ opts.add_argument("--headless")
+    # ~ global opts?
+    
+    global wd1, wd2, in_q, out_q
+
+    wd1 = webdriver.Firefox() # ~ webdriver.Firefox(executable_path="PATH", options=opts)
+    wd2 = webdriver.Firefox() # ~ Only wd2 should be headless, if any.
+    
+    in_q = queue.Queue(maxsize=20)
+    out_q = queue.Queue(maxsize=20)
+
+    return wd1, wd2, in_q, out_q
+
+
 def main():
+    pickAcct()
+    
+    makeObjects()
+
     getAdmin()
-    getViewer()
 
-    t1 = threading.Thread(target=searcher, name="Thread 1", args=(in_q, out_q,), daemon=True)
-    t1.start()
+    getViewer(acct)
 
-    t2 = threading.Thread(target=connecter, name="Thread 2", args=(out_q,), daemon=True)
-    t2.start()
+    t1 = threading.Thread(
+        target=searcher, name="Thread 1", args=(in_q, out_q,), daemon=True
+    ).start()
 
-    print("\nScan or enter bridge serials."
-         "\nMultple serials can be copy/pasted from spreadsheet at one time, also.")
+    t2 = threading.Thread(
+        target=connecter, name="Thread 2", args=(out_q,), daemon=True
+    ).start()
+
+    print("\nProgram is ready. Enter bridge serials as needed (w/o prompt).\n"
+          "\nBest practice is to copy-paste multiple serials at a time.\n\n")
 
     while True:
-        x = input().upper()
+        serial = input().upper()
 
-        if (x[0:6] == "EEN-BR") and (len(x) == 16):
+        if (serial[0:6] == "EEN-BR") and (len(serial) == 16):
             try:
-                in_q.put(x)
+                in_q.put(serial)
             except queue.Full:
                 continue
             else:
                 continue
 
-        print("\n", x, "does not match proper format: EEN-BRXXX-XXXXXX\n")
+        print("\n", serial, "does not match proper format: EEN-BRXXX-XXXXXX\n")
         continue
+        
 
-
-while True:
-    option = input(
-        "\nWhich Eagle Eye Viewer account will be used?"
-        "\n1. ACCOUNT  OR  2. ACCOUNT2  [ACC/ACC2]: "
-    ).upper()
-
-    if option == 1 or "SUT":
-        acct = ACCOUNT
-        break
-
-    elif option == 2 or "SUT2":
-        acct = ACCOUNT2
-        break
-
-    print("\nGot invalid response:", str(option), "\n")
-    continue
-
-# ~ options = webdriver.FirefoxOptions()
-# ~ options.add_argument("--headless")
-
-wd1 = webdriver.Firefox()
-wd2 = webdriver.Firefox()
-
-in_q = queue.Queue()
-out_q = queue.Queue()
-
-main()
+if __name__ == "__main__":
+    main()
