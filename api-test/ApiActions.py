370cb9a4dfd95c8b03b9b56569579597493918eb
Oscar Alvidrez <oalvidrez@een.com>
2023-05-25 10:28:39 -0500

    Added login method

--- api-test/ApiActions.py
+++ api-test/ApiActions.py
@@ -15,5 +15,29 @@ class ApiActions(object):
         # Session setup
         self.base_url = "https://login.eagleeyenetworks.com"
 
+    def login(self, username: str, password: str):
+        # Define Payload and POST
+        payload = {"username": username, "password": password}
+        response = self.rs.post(self.base_url + "/g/aaa/authenticate", data = payload)
+        assert response.status_code == 200
+
+        # Define payload and authenticate token
+        payload = {"token": response.json()['token']}
+        response = self.rs.post(self.base_url + "/g/aaa/authorize", data = payload)
+        assert response.status_code == 200
+
+        # Set auth_key
+        self.auth_key = response.cookies['auth_key']
+
+        # Set user object data
+        self.user_object = response.json()
+
+        # Update base URL with active subdomain
+        self.base_url = self.base_url.replace('login', self.user_object['active_brand_subdomain'])
+        print(self.base_url)
+
+        return self.rs
+
 if __name__ == '__main__':
     api = ApiActions()
+    api.login("username","password")
