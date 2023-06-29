62bc3008c0364c43433b9949d744ee50bb81f8a4
Fabian Bryant <134044947+fabianbryant@users.noreply.github.com>
2023-06-28 19:43:08 -0500

    Update AdminActions.py
    Functional.

--- admin-test/AdminActions.py
+++ admin-test/AdminActions.py
@@ -19,13 +19,14 @@ from selenium.common.exceptions import (
 class AdminActions(object):
     def __init__(self):
         # Create webdriver
-        self.wd = webdriver.Firefox()
+        self.driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
 
-        # Create search queue
-        self.q = queue.Queue(maxsize=20)
+        # Create I/O queues
+        self.in_q = queue.Queue(maxsize=20)
+        self.out_q = queue.Queue(maxsize=20)
 
         # Create searcher thread
-        self.th = Thread(target=self.search, args=(self, q), daemon=True)
+        self.thread = Thread(target=self.search, args=(self,), daemon=True)
 
     def login(self):
         tries = 0
@@ -41,11 +42,11 @@ class AdminActions(object):
                 continue
 
             # GET EEN Admin website
-            self.wd.get("https://eenadmin.eagleeyenetworks.com")
+            self.driver.get("https://eenadmin.eagleeyenetworks.com")
 
             # Break loop if already logged in
             try:
-                Wait(self.wd, 2.5).until(
+                Wait(self.driver, 2.5).until(
                     ec.visibility_of_element_located((By.NAME, "search_type"))
                 )
             except (Timeout, NotFound):
@@ -55,7 +56,7 @@ class AdminActions(object):
 
             # Prompt user to manually log in
             try:
-                Wait(self.wd, 7.5).until(
+                Wait(self.driver, 7.5).until(
                     ec.visibility_of_element_located((By.NAME, "username"))
                 )
             except (Timeout, NotFound):
@@ -69,7 +70,7 @@ class AdminActions(object):
                     continue
                 break
 
-    def search(self, q):
+    def search(self, serial: str):
         tries = 0
 
         # Search loop for retrieving and handling bridge data
@@ -84,7 +85,7 @@ class AdminActions(object):
 
             # Check search-queue and GET
             try:
-                serial = self.q.get()
+                serial = self.in_q.get()
             except queue.Empty():
                 time.sleep(0.5)
                 continue
@@ -94,45 +95,46 @@ class AdminActions(object):
 
             # Select search-type and search
             try:
-                Select(Wait(self.wd, 5).until(
+                Select(Wait(self.driver, 5).until(
                     ec.visibility_of_element_located((By.NAME, "search_type"))
                 )).select_by_visible_text("Bridges and Cameras")
             except (Timeout, NotFound):
-                self.q.put(serial)
+                self.in_q.put(serial)
                 continue
             else:
-                srch = self.wd.find_element(By.NAME, "search")
+                srch = self.driver.find_element(By.NAME, "search")
                 srch.clear()
                 srch.send_keys(serial, Keys.RETURN)
 
             # Report no matches if none and continue
             try:
-                Wait(self.wd, 2.5).until(
+                Wait(self.driver, 2.5).until(
                     ec.visibility_of_element_located((By.XPATH, ".//p[contains(text(), 'No matches.')]"))
-                    )
+                )
             except (Timeout, NotFound, InvalidSel):
                 pass
             else:
                 print(f"\nNo matches found for '{serial}'.\n")
                 continue
-            
+
             # Retrieve and display bridge data
             try:
-                ip_addr = Wait(self.wd, 5).until(
+                ip_addr = Wait(self.driver, 5).until(
                     ec.visibility_of_element_located((By.XPATH, "//td[@id='bridge-ip_address']"))
                 ).text
             except (Timeout, NotFound, InvalidSel):
                 continue
             else:
-                serial = self.wd.find_element(By.XPATH, "//td[@id='bridge-serial']").text
-                attach_id = self.wd.find_element(By.XPATH, "//td[@id='bridge-connect_id']").text
-                firmware = self.wd.find_element(By.XPATH, "//td[@id='bridge-firmware']").text
+                serial = self.driver.find_element(By.XPATH, "//td[@id='bridge-serial']").text
+                attach_id = self.driver.find_element(By.XPATH, "//td[@id='bridge-connect_id']").text
+                firmware = self.driver.find_element(By.XPATH, "//td[@id='bridge-firmware']").text
 
-                print(f"\n\t\t{ip_addr}\n\n\t\t{serial}\n\n\t\t{attach_id}\n\n\t\t{firmware}\n")
+                print(f"\n\t\tIP: {ip_addr}\n\n\t\tSerial: {serial}"
+                      f"\n\n\t\tAttach ID: {attach_id}\n\n\t\tFirmware: {firmware}\n")
 
             # PUT relevant bridge data into outbound queue
             try:
-                q.put_nowait((attach_id, serial))
+                self.out_q.put_nowait((attach_id, serial))
             except queue.Full():
                 pass
 
@@ -147,23 +149,23 @@ class AdminActions(object):
             else:
                 with open(file_name + ".csv", "a") as csv_file:
                     writer = csv.writer(csv_file, delimiter="\t")
-                    writer.writerow(serial, attach_id, firmware)
+                    writer.writerow((serial, attach_id, firmware))
 
             # Reset attempts and continue
             tries = 0
 
     def start(self):
         # Start searcher thread
-        self.th.start()
+        self.thread.start()
 
         print("\n...Ready! Scan-in or enter one or multiple bridge serials at a time.\n")
-        
+
         # Loop for entering bridge serials for searching
         while True:
             serial = input().upper()
             if (serial[0:6] == "EEN-BR" and len(serial) == 16) or (serial[0:5] == "MX-BR" and len(serial) == 15):
                 try:
-                    self.q.put(serial)
+                    self.in_q.put(serial)
                 except queue.Full:
                     continue
                 else:
@@ -175,5 +177,4 @@ class AdminActions(object):
 if __name__ == "__main__":
     admin = AdminActions()
     admin.login()
-    q = queue.Queue(maxsize=1)  # Maxsize 1 b/c outbound queue is useless without ApiActions.py or ViewerActions.py
     admin.start()
